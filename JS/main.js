e5a79e8f7056ecdeb932a4ba209575eee74b4583
h.anter <h.anter@IT-L174>
2023-11-29 10:31:04 +0200

    Add project files.

---
+++ JS/main.js
@@ -0,0 +1,11 @@
+let d=document.querySelector("button");
+let pr=document.querySelector(".test");
+let tr=document.querySelector(".try");
+console.log(tr);
+
+
+
+d.onclick=function(){
+    tr.classList.add("hidden");
+    pr.classList.add("pppp");
+}
